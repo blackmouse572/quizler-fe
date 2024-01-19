f67920d6b2e83d9d49ec249c3e1a54c6573697c0
lilinkw <kjlinh098@gmail.com>
2024-01-19 17:53:15 +0700

    QLN-3 feat: display not found page on the UI

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,13 +1,17 @@
-import { Input } from "@/components/ui/input"
+import { useTranslations } from "next-intl"
 
+import { Input } from "@/components/ui/input"
 export default function NotFound() {
+  const t = useTranslations("NotFoundPage")
   return (
     <div className="flex h-screen w-screen flex-row items-center justify-center text-center">
       <div className="flex flex-col space-y-11">
-        <h2 className="text-4xl font-extrabold">You seem lost?</h2>
-        <p className="font-medium">The page you seeks seem like terminated or not existed 😎😎</p>
+        <h2 className="text-4xl font-extrabold">{t('title')}</h2>
+        <p className="font-medium">
+          {t('message')}
+        </p>
         {/* <Link href="/">Return Home</Link> */}
-        <Input placeholder="New quiz"/>
+        <Input placeholder={t('input.placeholder')} />
       </div>
     </div>
   )
