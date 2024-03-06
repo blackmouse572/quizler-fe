9d098751a888e74c591e05147492fe62e2f0088d
lilinkw <kjlinh098@gmail.com>
2024-03-06 23:42:41 +0700

    [QLN-67] add API get classroom data

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -14,3 +14,43 @@ export function getUserProfile(token: string) {
     return response.json()
   })
 }
+
+export function fetchClassroomByUserId(token: string, userId: string) {
+  const URL = getAPIServerURL(`/classrooms/getbyaccountid/${userId}`)
+
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    }
+  }
+
+  return fetch(URL, options).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+    return response.json()
+  })
+
+}
+
+export function fetchClassroomCurrentUser(token: string) {
+  const URL = getAPIServerURL(`/classrooms/getcurrent`)
+
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    }
+  }
+
+  return fetch(URL, options).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+    return response.json()
+  })
+
+}
