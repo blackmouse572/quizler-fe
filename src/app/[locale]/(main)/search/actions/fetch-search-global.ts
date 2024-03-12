6d4b32f0eb538416207ac61b2a009284bbc918ca
pikaden <87664605+pikaden@users.noreply.github.com>
2024-03-12 18:03:02 +0700

    [QLN-112] refactor: refactor types

--- src/app/[locale]/(main)/search/actions/fetch-search-global.ts
+++ src/app/[locale]/(main)/search/actions/fetch-search-global.ts
@@ -1,13 +1,26 @@
 import { getAPIServerURL } from "@/lib/utils"
 
-export const fetchSearchGlobal = async (searchQuery: string) => {
-  const URL = getAPIServerURL(`/searchglobal/searchglobal?search=${searchQuery}`)
+export const fetchSearchGlobal = async (
+  searchQuery: string,
+  take: string | null,
+  skip: string | null
+) => {
+  var apiUrl = `/searchglobal/searchglobal?search=${searchQuery}`
+
+  if (take) {
+    apiUrl += `&take=${take}`
+  }
+  if (skip) {
+    apiUrl += `&skip=${skip}`
+  }
+
+  const URL = getAPIServerURL(apiUrl)
 
   const options = {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-    }
+    },
   }
 
   const res = await fetch(URL, options).then(async (response) => {
@@ -19,4 +32,4 @@ export const fetchSearchGlobal = async (searchQuery: string) => {
   })
 
   return res
-} 
+}
