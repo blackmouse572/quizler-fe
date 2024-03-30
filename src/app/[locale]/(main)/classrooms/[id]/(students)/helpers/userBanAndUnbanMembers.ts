54a7f1b2e530ea627e01ec44b77b12dc1e610f0b
lilinkw <kjlinh098@gmail.com>
2024-03-30 15:59:12 +0700

    [QLN-29] feat: call API to ban and unban user

---
+++ src/app/[locale]/(main)/classrooms/[id]/(students)/helpers/userBanAndUnbanMembers.ts
@@ -0,0 +1,36 @@
+import { useMutation } from "@tanstack/react-query"
+import {
+  TBanAPIProps,
+  banAndUnbanMemberAction,
+} from "../actions/ban-unban-member-action"
+
+type Props = {
+  onSuccess?: () => void
+  onError?: (error: Error) => void
+}
+
+export function useBanAndUnbanBatchMember({ onSuccess, onError }: Props) {
+  return useMutation({
+    mutationFn: async ({ memberIds, classroomId, action }: TBanAPIProps) => {
+      debugger
+      const res = await banAndUnbanMemberAction({
+        memberIds: memberIds,
+        classroomId: classroomId,
+        action,
+      })
+      if (!res.ok) {
+        throw new Error(res.message)
+      } else {
+        return res.data
+      }
+    },
+    onSettled: (data, error) => {
+      if (error) {
+        console.error(error)
+      } else {
+        onSuccess?.()
+      }
+    },
+    onError,
+  })
+}
