97e65c9fabb34ab5942c294444df24e4fbc9fbf6
pikaden <87664605+pikaden@users.noreply.github.com>
2024-04-01 23:15:34 +0700

    [QLN-193] feat: view, delete quizbank content

--- src/app/[locale]/admin/quizbank/page.tsx
+++ src/app/[locale]/admin/quizbank/page.tsx
@@ -1,11 +1,9 @@
-import { getAPIServerURL } from "@/lib/utils"
-import QuizBank from "@/types/QuizBank"
-import PagedRequest from "@/types/paged-request"
-import PagedResponse from "@/types/paged-response"
 import _ from "lodash"
 import { NextIntlClientProvider } from "next-intl"
 import { getMessages, getTranslations } from "next-intl/server"
 import { QuizBankTable } from "./components/table"
+import getAllQuizBanksAction from "./actions/get-all-quizbanks-action"
+import { notFound } from "next/navigation"
 
 type AdminQuizBankProps = {
   searchParams: { [key: string]: string | string[] | undefined }
@@ -27,34 +25,6 @@ export async function generateMetadata({
   }
 }
 
-async function getQuizBank(options: Partial<PagedRequest>) {
-  //Convert object to query string
-  const params = new URLSearchParams()
-
-  for (const [key, value] of Object.entries(options)) {
-    if (value !== undefined) {
-      params.set(key, String(value)) // Ensure value is a string
-    }
-  }
-  const option: RequestInit = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    next: {
-      revalidate: 1, // Revalidate every 1 second
-    },
-  }
-  const url = getAPIServerURL("/QuizBank") + "?" + params
-  const res: PagedResponse<QuizBank> = await fetch(url, option)
-    .then((res) => res.json())
-    .catch((err) => {
-      console.error(`[ERROR] getQuizBank: ${url} `, err)
-      throw new Error(err)
-    })
-  return res
-}
-
 async function AdminQuizBankPage({ searchParams }: AdminQuizBankProps) {
   const take = searchParams.take ? parseInt(searchParams.take as string) : 20
   const skip = searchParams.skip ? parseInt(searchParams.skip as string) : 0
@@ -62,15 +32,19 @@ async function AdminQuizBankPage({ searchParams }: AdminQuizBankProps) {
     ? encodeURIComponent(searchParams.search as string)
     : undefined
   const options = { take, skip, search }
-  const data = await getQuizBank(options)
+  const data = await getAllQuizBanksAction({
+    filter: options
+  })
   const messages = await getMessages()
+
+  if (!data.ok || !data.data) notFound()
 
   return (
     <div className="">
       <NextIntlClientProvider
-        messages={_.pick(messages, "Table", "QuizBankAdmin", "Validations")}
+        messages={_.pick(messages, "Table", "QuizBankAdmin", "Validations", "Errors")}
       >
-        <QuizBankTable data={data} />
+        <QuizBankTable data={data.data} />
       </NextIntlClientProvider>
     </div>
   )
