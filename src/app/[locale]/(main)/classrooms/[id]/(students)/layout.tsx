8b42b2a9611fb7df9bf9ea9df466c4dd0652c44b
blackmouse572 <Ngocvlqt1995@gmail.com>
2024-04-14 20:38:33 +0700

    [QLN-326] fix: not authro cannot see classroom member

--- src/app/[locale]/(main)/classrooms/[id]/(students)/layout.tsx
+++ src/app/[locale]/(main)/classrooms/[id]/(students)/layout.tsx
@@ -1,6 +1,9 @@
+import getClassroomDetails from "@/app/[locale]/(main)/classrooms/actions/get-classroom-details-action"
+import { getUser } from "@/lib/auth"
 import _, { capitalize } from "lodash"
 import { NextIntlClientProvider } from "next-intl"
 import { getMessages } from "next-intl/server"
+import { notFound } from "next/navigation"
 import { NavigationBar } from "./components/navigation-bar"
 
 enum ChildURL {
@@ -18,9 +21,21 @@ export default async function Layout({
   children: React.ReactNode
 }) {
   const messages = await getMessages()
+  const user = getUser()
+  const childURL = ["members", "manage"]
+  const routes = childURL.map((url) => ({
+    url: `/classrooms/${id}/${url}`,
+    name: capitalize(url),
+  }))
 
-  const childURL = ['members', 'manage']
-  const routes = childURL.map(url => ({url: `/classrooms/${id}/${url}`, name: capitalize(url)}))
+  const data = await getClassroomDetails(id)
+  if (!data.ok) {
+    throw Error(data.message)
+  }
+
+  if (!data.data || !user || data.data.author.id !== user.id) {
+    return notFound()
+  }
 
   return (
     <>
