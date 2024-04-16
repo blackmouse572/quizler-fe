be87770b2cb1789a8056acf06c7fb86a0f63552b
pikaden <87664605+pikaden@users.noreply.github.com>
2024-04-16 17:21:35 +0700

    feat: add loading pages
    I want to swear, but I will not.
    Added all loading page (hopefully so)

---
+++ src/app/[locale]/(auth)/signup/verify/loading.tsx
@@ -0,0 +1,50 @@
+import {
+    Card,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  import { Skeleton } from "@/components/ui/skeleton"
+  
+  function VerifyPasswordLoadingPage() {
+    return (
+      <div className="h-screen space-y-2">
+        <div className="grid h-72">
+          <Card className="h-full">
+            <CardHeader>
+              <CardTitle>
+                <Skeleton className="h-8 w-full" />
+              </CardTitle>
+              <CardDescription>
+                <Skeleton className="h-5 w-full" />
+                <Skeleton className="h-5 w-full" />
+              </CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <Skeleton className="h-5 w-full" />
+            </CardFooter>
+          </Card>
+        </div>
+        <div className="grid h-72 pt-10">
+          <Card className="h-full">
+            <CardHeader>
+              <CardTitle>
+                <Skeleton className="h-8 w-full" />
+              </CardTitle>
+              <CardDescription>
+                <Skeleton className="h-5 w-full" />
+                <Skeleton className="h-5 w-full" />
+              </CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <Skeleton className="h-5 w-full" />
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    )
+  }
+  
+  export default VerifyPasswordLoadingPage
+  
